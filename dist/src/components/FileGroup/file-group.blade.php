fdde7e52b59a4f357566424e8695e7d9562a2daa
Leesa Ward <leesa@doubleedesign.com.au>
2025-07-30 12:46:10 +1000

    Export standalone packages in CI instead of copying things locally; refactor into one script

--- dist/src/components/FileGroup/file-group.blade.php
+++
@@ -1,7 +0,0 @@
-<div @if ($classes) @class($classes) @endif @attributes($attributes) role="group">
-    @foreach ($children as $child)
-        @if (method_exists($child, 'render'))
-            {{ $child->render() }}
-        @endif
-    @endforeach
-</div>
